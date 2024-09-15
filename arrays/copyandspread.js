c9bcce92f2887a404853c01c8683bb67f64be0b6
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-15 11:55:46 +0530

    destructuring

---
+++ arrays/copyandspread.js
@@ -0,0 +1,4 @@
+let arr1 = [10, 20, 30];
+let arr2 = [...arr1,'something'];
+console.log(arr1);
+console.log(arr2);
---
+++ arrays/destructuring.js
@@ -0,0 +1,5 @@
+let arr1 = ['hello','bhavesh','how'];
+let [item1, ...item2] = arr1;
+
+console.log(item1);
+console.log(item2);
