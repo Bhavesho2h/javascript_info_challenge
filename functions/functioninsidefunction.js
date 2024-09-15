f4cd43dba983c220de5d7b902a6972584f5fc473
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-15 23:19:18 +0530

    functioninsidefunction

--- functions/functioninsidefunction.js
+++ functions/functioninsidefunction.js
@@ -1,5 +1,6 @@
 const fun = () => {
     console.log('this is first function');
+    debugger;
     const fun2 = () => {
         console.log('this is second function');
     }
---
+++ topic_4_objects/objectinsidearray.js
@@ -0,0 +1,9 @@
+let arr1 = [
+    { name: 'bhavesh', age: 23 },
+    { name: 'purohit', age: 23 },
+    { name: 'john', age: 21 }
+]
+
+arr1.map((value)=>{
+    console.log(value.name,'value');
+})
