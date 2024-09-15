a9943a4d245b8acf889927027178f57edfd9b617
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-15 23:51:35 +0530

    this keyword

---
+++ javascript_part3/this.js
@@ -0,0 +1,15 @@
+// let obj = {
+//     name: 'bhavesh',
+//     age: 22,
+//     fun: function () {
+//         console.log(this.name);
+//     }
+// }
+
+// obj.fun();
+
+function fun() {
+    console.log(this);
+}
+
+fun();
