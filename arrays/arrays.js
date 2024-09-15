5e107d0bbaf96d096c35d3ce87a6ac9b6a2e1b5f
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-15 10:17:48 +0530

    arrays in js

---
+++ arrays/arrays.js
@@ -0,0 +1,18 @@
+let name = ['hello', 'my', 'name', 1, true];
+
+//shift:removes first elements from the array
+// name.shift()
+
+//unshift:
+// name.unshift("bhavesh");
+
+//splice:- remove two elements from first index
+// name.splice(1,2);
+
+// name.splice(1,1,'bhavesh');
+
+///slice method:-
+
+let a = name.slice(1,3);
+
+console.log(a);
