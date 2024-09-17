9bf54a91e44bd62327d991c368cc9f1fa72a819b
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-18 00:32:35 +0530

    newkeyword

---
+++ javascript_part4/newkeyword.js
@@ -0,0 +1,18 @@
+function createUser(username, score) {
+    this.username = username;
+    this.score = score;
+
+}
+
+createUser.prototype.increment = function() {
+    this.score++;
+}
+
+createUser.prototype.printMe = function() {
+ console.log(this.score);   
+}
+
+const chai = new createUser("chai",25); //jab tak new nahi dalenge tab tak kuch inject nahi hua hoga sab hava me hoga
+const tea = createUser('tea',250);
+
+chai.printMe();
