12593041df7495ce88724702a78f6c1204eecf6d
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-18 13:47:48 +0530

    jira project started

--- javascript_part4/static.js
+++ javascript_part4/static.js
@@ -2,11 +2,15 @@ class Animal{
     constructor(name){
         this.name = name;
     }
-   static sayHi(){
-        console.log(`my name is ${this.name}`);
+   static sayHi(name){
+        console.log(`my name is ${name}`);
     }
 }
 
-let user = new Animal('bhavesh');
-user.sayHi();
+// let user = new Animal('bhavesh');
+// user.sayHi();
 
+
+//now how to access static objects
+let name = 'cow'
+Animal.sayHi(name);
