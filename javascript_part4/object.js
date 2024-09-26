e99221c1f265dc2e403e4655377b7fc02444f5c6
Bhavesho2h <bhavesh.purohit@o2h.com>
2024-09-27 01:14:10 +0530

    ticket added with task

--- javascript_part4/object.js
+++ javascript_part4/object.js
@@ -6,3 +6,4 @@ something.power = 2; // so here we can say that function is also an object
 
 console.log(something.power);
 
+
--- jira/script.js
+++ jira/script.js
@@ -1,12 +1,44 @@
 let addBtn = document.querySelector(".add-btn");
 let modal = document.querySelector(".modal-cont");
+let textAreaCont = document.querySelector(".textarea-cont");
 let addModal = true;
-addBtn.addEventListener('click',function(){
-    if(addModal){
+let taskArea = document.querySelector(".task-area");
+let mainCont = document.querySelector(".main-cont");
+let newDiv = document.createElement('div');
+let ticketCont = document.querySelector("ticket-cont");
+let ticketColor = document.querySelector(".ticket-color");
+let ticketId = document.querySelector('.ticket-id');
+
+
+addBtn.addEventListener('click', function () {
+    if (addModal) {
         modal.style.display = 'flex'
     }
-    else{
+    else {
         modal.style.display = 'none'
     }
     addModal = !addModal;
 })
+
+modal.addEventListener('keydown', (e) => {
+    let key = e.key;
+    if (key == 'Enter') {
+        createTicket(textAreaCont.value);
+        modal.style.display = 'none';
+        textAreaCont.value = "";
+        addModal = !addModal
+    }
+})
+
+function createTicket(value) {
+    let ticketCont = document.createElement('div');
+    ticketCont.setAttribute('class','ticket-cont')
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+          <div class="ticket-id">#8yh68gh</div>
+          <div class="task-area">${value}</div>`
+
+    mainCont.appendChild(ticketCont)
+
+
+
+}
